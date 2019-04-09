dd372a61b4de55caa629998d7df49e039de6c241
worldandme <1007330936@qq.com>
2019-04-09 11:53:07 +0800

    简书init，header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from './common/header';
+class App extends Component {
+  render() {
+    return (
+      <div className=''>
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
