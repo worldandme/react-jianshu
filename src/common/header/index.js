e94672b45a4536f4491603c5216e25f8f9c30823
worldandme <1007330936@qq.com>
2019-04-09 16:50:52 +0800

    搜索块隐藏消失功能

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -12,8 +12,35 @@ import {
   NavSearch,
   Addition,
   Button,
-  SearchWrapper
+  SearchWrapper,
+  SearchInfo,
+  SearchInfoTitle,
+  SearchInfoSwitch,
+  SearchInfoItem,
+  SearchInfoList
 } from './style'
+
+const getListArea = (show) => {
+  if(show){
+    return (
+      <SearchInfo >
+            <SearchInfoTitle>
+              热门搜索
+              <SearchInfoSwitch>换一批</SearchInfoSwitch>
+            </SearchInfoTitle>
+            <SearchInfoList>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+            </SearchInfoList>
+          </SearchInfo>
+    )
+  }else{
+    return null
+  }
+}
 
 const Header = (props) => {
   return (
@@ -40,6 +67,7 @@ const Header = (props) => {
             </NavSearch>  
           </CSSTransition>
           <i  className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe637;</i>
+          {getListArea(props.focused)}
         </SearchWrapper>
         
       </Nav>
